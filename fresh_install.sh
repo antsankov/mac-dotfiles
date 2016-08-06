848cb12f4ed636bcbaed9b60f00f44c3c6c054cf
Alex Tsankov <antsankov@gmail.com>
2016-08-05 22:14:36 -0400

    removed unnecessary parts

---
+++ fresh_install.sh
@@ -0,0 +1,29 @@
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# install packages
+brew install zsh python python3 tmux wget brew-cask
+
+
+# install prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+chsh -s /bin/zsh
+
+# Symlink all dotfiles to home directories
+
+
+# install importnt gui apps
+brew cask install google-chrome iterm2 vlc google-drive
+
+
+# iterm solarized dark
+wget https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
+
+
+# install fonts 
+git clone https://github.com/powerline/fonts && ./fonts/install.sh && rm -rf fonts
