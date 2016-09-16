6540503d694d675daaf78922f94f391fbfddbb59
Alex Tsankov <alextsankov@wf14316.local>
2016-09-16 11:27:17 -0600

    Modified with new vim config

--- fresh_install.sh
+++ fresh_install.sh
@@ -3,9 +3,8 @@
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-
 # install packages
-brew install zsh python python3 tmux wget brew-cask wget
+brew install zsh python python3 tmux wget brew-cask wget vim
 brew link wget
 
 # install prezto
@@ -16,16 +15,18 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 chsh -s /bin/zsh
 
-# Symlink all dotfiles to home directories
-ln config/.* ~
 
 # install importnt gui apps
 brew cask install google-chrome iterm2 vlc google-drive alfred flux atom bettertouchtool
 
-
 # iterm solarized dark
 wget https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
 
-
 # install fonts
 git clone https://github.com/powerline/fonts && ./fonts/install.sh && rm -rf fonts
+
+# install vim
+git clone https://github.com/ddellaquila/dd-vim.giti && cd dd-vim && ./install.sh
+
+# Symlink all dotfiles to home directories
+ln config/.* ~
